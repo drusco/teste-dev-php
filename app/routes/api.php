60f7ade078802102f9dce74b976c08cd0fa7f95f
Pedro Gallardo <pedrogallardo7011@gmail.com>
2024-12-15 23:46:40 -0300

    fix: enable api routing from artisan command

--- app/routes/api.php
+++ app/routes/api.php
@@ -1,5 +1,17 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\SupplierController;
 
-Route::apiResource('suppliers', SupplierController::class);
+Route::middleware('api')->group(function () {
+    Route::get('/suppliers', [SupplierController::class, 'index']);
+    Route::post('/suppliers', [SupplierController::class, 'store']);
+    Route::get('/suppliers/{id}', [SupplierController::class, 'show']);
+    Route::put('/suppliers/{id}', [SupplierController::class, 'update']);
+    Route::delete('/suppliers/{id}', [SupplierController::class, 'destroy']);
+});
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
